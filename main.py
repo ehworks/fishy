8bf373a1c96195de9b7d44a944d36e581f5916fe
ehworks <87453954+ehworks@users.noreply.github.com>
2022-03-22 20:08:45 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,77 @@
+from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+
+
+import smtplib
+import requests
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField(label='Log In')
+
+
+app = Flask(__name__)
+app.secret_key = "BST1686tx"
+
+
+G_EMAIL = "day.32.practice@gmail.com"
+G_PASSWORD = "zavnWKkvFVmE9d8"
+Y_EMAIL = "day_32_practice@yahoo.com"
+Y_PASSWORD = "gg36H2Pgz38gjQJ"
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route("/hobbies.html")
+def hobbies():
+    return render_template("hobbies.html")
+
+
+@app.route("/contact.html")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/login.html")
+def login():
+    login_form = LoginForm()
+    return render_template('login.html', form=login_form)
+
+
+#
+# @app.route('/login', methods=["POST"])
+# def receive_data():
+#     name = request.form["username"]
+#     password = request.form["password"]
+#     typefish = request.form["typeFish"]
+#
+#     conditions = request.form["yourDescribe"]
+#
+#     return f"<h1> Name: {name} </h1> <br> <h1>Type of Fish: {typefish} </h1> " \
+#            f"<br> <h1>Conditions are: {conditions} </h1>"
+
+
+# def send_email():
+#     with smtplib.SMTP("smtp.gmail.com") as connection:
+#         # with smtplib.SMTP("smtp.gmail.com") as connection:
+#         # simple mail transfer protocol
+#         connection.starttls()
+#         connection.login(G_EMAIL, G_PASSWORD)
+#         connection.sendmail(
+#                 from_addr=Y_EMAIL,
+#                 to_addrs="day.32.practice@gmail.com",
+#                 msg="Subject:Want to see Space Station \n\n this is the body ")
+#
+
+if __name__ == '__main__':
+
+    app.run(debug=True)
+
+# send_email()
