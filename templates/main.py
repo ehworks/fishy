58d9710c9b88b3463c0064f0f00c2622beb388bb
ehworks <87453954+ehworks@users.noreply.github.com>
2022-09-11 16:32:13 -0500

    Update main.py

--- templates/main.py
+++ templates/main.py
@@ -1,7 +1,4 @@
 from flask import Flask, render_template
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
 import datetime
 
 app = Flask(__name__)
