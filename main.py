e6378697720e21bafcd94077f3de491a05034ff3
ehworks <87453954+ehworks@users.noreply.github.com>
2022-09-14 19:05:08 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, redirect
 import datetime
 from twilio.twiml.messaging_response import MessagingResponse
 
+
+
 app = Flask(__name__)
 
 
@@ -11,17 +13,22 @@ def home():
     return render_template('index.html', year=current_year)
 
 
-@app.route("/sms", methods=['POST'])
-def sms_reply():
-    """Respond to incoming call with text message"""
-    # Start TwiML response
+@app.route("/sms", methods=['GET', 'POST'])
+def incoming_sms():
+    """Send a dynamic reply to an incoming text message"""
+    # Get the message the user sent our Twilio number
+    body = request.values.get('Body', None)
+
+    # Start our TwiML response
     resp = MessagingResponse()
 
-    # Add a message
-    resp.message("Thanks for your interest, Really")
+    # Determine the right reply for this message
+    if body == 'hello':
+        resp.message("Hi!")
+    elif body == 'bye':
+        resp.message("Goodbye")
 
     return str(resp)
-
 
 
 @app.route("/hobbies.html")
